0331804f6859f5ad50d66ed8ce3aa527c9f00072
Tiago Oliveira <tiagod_oliveira@hotmail.com>
2020-01-09 03:11:48 +0000

    Updates

---
+++ precache-manifest.98b1564fc44d806f934224027c1c436b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94dcb065dacce0640560caeff643d4ef",
+    "url": "/tiagodoliveira/man-challenge/index.html"
+  },
+  {
+    "revision": "d6999a33188dcf220b18",
+    "url": "/tiagodoliveira/man-challenge/static/css/main.bf6a68e0.chunk.css"
+  },
+  {
+    "revision": "9dfcb5cb9e157bb01035",
+    "url": "/tiagodoliveira/man-challenge/static/js/2.a9e79757.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tiagodoliveira/man-challenge/static/js/2.a9e79757.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d6999a33188dcf220b18",
+    "url": "/tiagodoliveira/man-challenge/static/js/main.5dd5cf0c.chunk.js"
+  },
+  {
+    "revision": "2bfd0d2fb14395103c47",
+    "url": "/tiagodoliveira/man-challenge/static/js/runtime-main.6dd874a1.js"
+  }
+]);
